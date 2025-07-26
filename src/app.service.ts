6a5c825acc0149a01892f95c962c80ead6c5ef19
unknown <yufis0844@gmail.com>
2025-07-26 05:41:20 +0300

    deployed AWS

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello from LockMyTrades';
+    return 'Hello 👋 world!';
   }
 }
